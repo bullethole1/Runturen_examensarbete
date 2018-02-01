de1d6d4f48274cfd10872afddf66676dd5f4c54d
Johan Lund <johanjohan.lund@gmail.com>
2018-02-01 14:18:55 +0100

    removed placeholder numbers in checkout page

--- src/providers/flickr/flickr.ts
+++ src/providers/flickr/flickr.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class FlickrProvider {
-  getApiUrl : string = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=e07254bcad2ef73f42095bad04366579&user_id=152375700%40N05&format=json&nojsoncallback=1&auth_token=72157691682579445-a8aab774f49291f1&api_sig=a0ed97ae758e20d7c603d7ed76bf36ab";    
+  getApiUrl : string = "https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=554863d13e40cf56f98818113f4a05bc&user_id=152375700%40N05&format=json&nojsoncallback=1";    
   
   constructor(public http: Http) {
     console.log('Hello FlickrProvider Provider');
