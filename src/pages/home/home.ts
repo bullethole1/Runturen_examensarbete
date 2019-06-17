425cfe1504fa1482026d78d16897177a3c85505e
Johan Lund <johanjohan.lund@gmail.com>
2019-06-17 22:49:31 +0200

    generated new google maps api and commentet out firebase code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,39 +1,36 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AboutPage } from '../about/about';
-import { GalleryPage } from '../gallery/gallery';
-import { GoogleMapsPage } from '../google-maps/google-maps';
-import { ProductsPage } from '../products/products';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AboutPage } from "../about/about";
+import { GalleryPage } from "../gallery/gallery";
+import { GoogleMapsPage } from "../google-maps/google-maps";
+import { ProductsPage } from "../products/products";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {}
 
-  ionViewDidLoad(){
+  ionViewDidLoad() {
     //console.log(this.mapRef);
     this.showMap();
   }
 
   //Function to navigate to various pages
-  showMap()
-  {
+  showMap() {
     var classRef = this;
-    $(".aboutRef").click(function(){
+    $(".aboutRef").click(function() {
       classRef.navCtrl.push(AboutPage);
     });
-    $(".galleryRef").click(function(){
-      classRef.navCtrl.push(GalleryPage); 
+    $(".galleryRef").click(function() {
+      classRef.navCtrl.push(GalleryPage);
     });
-    $(".mapRef").click(function(){
-      classRef.navCtrl.push(GoogleMapsPage); 
+    $(".mapRef").click(function() {
+      classRef.navCtrl.push(GoogleMapsPage);
     });
-    $(".storeRef").click(function(){
-      classRef.navCtrl.push(ProductsPage); 
+    $(".storeRef").click(function() {
+      classRef.navCtrl.push(ProductsPage);
     });
   }
-
 }
