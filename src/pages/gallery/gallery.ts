dfa5504d248c2051ebb7e6a862732dd6fc1c4489
Johan Lund <johanlund@Johans-MacBook-Pro.local>
2019-11-11 20:57:12 +0100

    final commit before secong release

--- src/pages/gallery/gallery.ts
+++ src/pages/gallery/gallery.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
 import { FlickrProvider } from '../../providers/flickr/flickr';
 
 @IonicPage()
@@ -9,19 +9,19 @@ import { FlickrProvider } from '../../providers/flickr/flickr';
 })
 export class GalleryPage {
 
-  photo:any;
-  
+  photo: any;
+
   constructor(public navCtrl: NavController, public flickrProvider: FlickrProvider) {
-    this.photo = {photos: {photo:[]}};
+    this.photo = { photos: { photo: [] } };
     this.getPosts();
     console.log("Getting posts");
   }
 
   // Here we subscribe to flickr provider and store the data in photo variable
   getPosts() {
-    this.flickrProvider.getPosts().subscribe((data)=>{
+    this.flickrProvider.getPosts().subscribe((data) => {
       this.photo = data;
       console.log(data);
-  });
+    });
   }
 }
